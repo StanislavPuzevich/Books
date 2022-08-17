7beec35e16bba0d81c290936221006d3783c9fff
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-08-17 07:14:10 +0300

    Add files via upload

---
+++ Principles and Practice Using C++(Bjarne Stroustrup)/Unit 11/Task 10.cpp
@@ -0,0 +1,33 @@
+﻿//10. Напишите функцию vector<string> split(const string & s).возвращающую вектор подстрок аргумента s, разделенных пробельными символами.
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+std::vector<std::string> split(const std::string& s)
+{
+    std::vector<std::string> result;
+    std::istringstream is;
+    std::string temp;
+    is.str(s);
+
+    while (is >> temp)
+        result.push_back(temp);
+
+    return result;
+}
+
+int main()
+{
+    std::string str;
+    std::vector<std::string> result;
+
+    std::cout << "Enter the string: ";
+    std::getline(std::cin, str);
+
+    result = split(str);
+    for (std::string i : result)
+        std::cout << i << std::endl;
+
+    return 0;
+}
