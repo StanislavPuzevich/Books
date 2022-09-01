e791be5cbb7a318c7be1eb55322a146656c09d30
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-09-01 12:11:50 +0300

    Add files via upload

---
+++ Principles and Practice Using C++(Bjarne Stroustrup)/Unit 11/Task 11.cpp
@@ -0,0 +1,44 @@
+﻿// 11. Напишите функцию vector<string> split(const string& s, const string& w), 
+// возвращающую вектор подстрок аргумента s, разделенных пробельными символами, при условии, что в качестве пробельного 
+// символа могут использоваться как "обычные" пробельные символы, так и символы из строки w
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> split(const std::string& s, const std::string& w)
+{
+    std::string buf;
+    std::vector<std::string> result;
+
+    for (char i : s)
+    {
+        if (i == ' ' || (w.find(i) != std::string::npos))
+        {
+            if (!buf.empty())
+                result.push_back(buf);
+            buf.clear();
+            continue;
+        }
+        buf += i;
+    }
+
+    return result;
+}
+
+int main()
+{
+    std::string w = ",!-";
+    std::string s;
+    std::vector<std::string> res;
+
+    std::cout << "Enter the string: " << std::endl;
+    std::getline(std::cin, s);
+    res = split(s, w);
+
+    std::cout << "Result: " << std::endl;
+    for (std::string i : res)
+        std::cout << i << std::endl;
+
+    return 0;
+}
